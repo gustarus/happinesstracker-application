2641e1fe4aaaef2b4eec0001b425ca5b9270ea7b
Kondratenko Pavel <p.kondratenko@rambler-co.ru>
2017-07-25 17:30:36 +0300

    add ability to hide splash screen before first screen loaded in debug mode

--- application.js
+++ application.js
@@ -2,14 +2,20 @@
 
 import React, {Component} from 'react';
 
+// hide splash screen on debug mode: with this hack
+// we can see errors which happened before first scene loaded
+import SplashScreen from 'react-native-splash-screen'
+const debug = typeof __DEV__ !== 'undefined' && __DEV__;
+debug && SplashScreen.hide();
+
 // configure moment
 import moment from 'moment';
 import 'moment/locale/en-gb';
 moment.locale('en-gb');
 
-// configure application instance
-import {app} from '@core/instances';
+// boot the app
 import {defaults} from '@core/config';
+import {app} from '@core/instances';
 app.configure(defaults).boot();
 
 module.exports = app.settings.home.screen;
